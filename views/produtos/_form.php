528a7e9240cea520828ca7a7f437fb4002e2d334
Ana Carolina <anacarolinaanacletob@gmail.com>
2024-07-02 15:56:29 -0300

    modified

--- views/produtos/_form.php
+++ views/produtos/_form.php
@@ -14,20 +14,21 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'id')->textInput() ?>
 
-    <?= $form->field($model, 'descrição')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'descricao')->textarea(['rows' => 6]) ?>
 
     <?= $form->field($model, 'tamanho')->textInput() ?>
 
     <?= $form->field($model, 'cor')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'preço')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'preco')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'tag')->textInput(['maxlength' => true]) ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Salvar', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
 
 </div>
+ 
